25c4a4603abbfdabc06252a464d8e08d35ad570e
Bryan Krauss <bryan@button.is>
2021-12-17 15:58:22 -0800

    feat: adds JsonSchemaForm and assc. dependencies
    ref #6

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,9 +1,10 @@
 import { Header, Footer } from '@button-inc/bcgov-theme';
+import JsonSchemaForm from '@rjsf/semantic-ui';
 
 export default function Home() {
   return (
     <>
-
+      <JsonSchemaForm />
     </>
   )
 }
