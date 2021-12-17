6a327b8bae078afc9469c4dd1c8d5d53516b8c85
Bryan Krauss <bryan@button.is>
2021-12-17 13:17:24 -0800

    feat: imports bcgov SDK theme and adds test components
    ref #3

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,7 +1,36 @@
+import { Header, Footer } from '@button-inc/bcgov-theme';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header
+        header="main"
+        onBannerClick={function noRefCheck() { }}
+        title="Welcome to the Connectivity Engagement"
+      />
+      <Component {...pageProps} />
+      <Footer>
+        <ul>
+          <li>
+            <a href=".">
+              About
+            </a>
+          </li>
+          <li>
+            <a href=".">
+              Careers
+            </a>
+          </li>
+          <li>
+            <a href=".">
+              Privacy Policy
+            </a>
+          </li>
+        </ul>
+      </Footer>
+    </>
+  )
 }
 
 export default MyApp
